b2fa9c1b0cdf0d748f89f72eea2efd8684da3906
Julio Vargas <juliovgalvao@gmail.com>
2020-11-30 20:43:56 -0300

    Exercicio 1, Salario, Observações Implementadas.

--- exercicio1.py
+++ exercicio1.py
@@ -1,18 +1,3 @@
-# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
-# Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados
-# 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
-
-#     salário bruto.
-#     quanto pagou ao INSS.
-#     quanto pagou ao sindicato.
-#     o salário líquido.
-#     calcule os descontos e o salário líquido, conforme a tabela abaixo:
-#     + Salário Bruto : R$
-#     - IR (11%) : R$
-#     - INSS (8%) : R$
-#     - Sindicato ( 5%) : R$
-#     = Salário Liquido : R$
-
 
 import math
 
@@ -24,46 +9,27 @@ def trabalho():
         try:
             horas = float(input('Quantidade do salario por hora: '))
             trabalhador.append(horas)
-            horasTrabalhadas = float(input('Quantas horas trabalhadas?'))
-            trabalhador.append(horasTrabalhadas)
-        except:
+            horas_trabalhadas = float(input('Quantas horas trabalhadas?'))
+            trabalhador.append(horas_trabalhadas)
+        except ValueError:
             print('Insira um valor valido!')
         else:
             break
 
 
 def descontos():
-    salarioBruto = math.prod(trabalhador)
-    INSS = salarioBruto * (8/100)
-    IR = salarioBruto * (11/100)
-    sindicato = salarioBruto * (5/100)
-    salarioLiquido = salarioBruto - INSS - IR - sindicato    
-    
-    """
-    Se você printar dessa forma a identação é respeitada.
-    
-    print(f'Seu Salario Bruto é de R$ {round(salarioBruto)}\n'
-          f'Desconto do INSS de R$ {round(INSS)}\n'
-          f'Desconto do Imposto de Renda de R$ {round(IR)}\n'
+    salario_bruto = math.prod(trabalhador)
+    inss = salario_bruto * (8/100)
+    ir = salario_bruto * (11/100)
+    sindicato = salario_bruto * (5/100)
+    salario_liquido = salario_bruto - inss - ir - sindicato
+    print(f'Seu Salario Bruto é de R$ {round(salario_bruto)}\n'
+          f'Desconto do INSS de R$ {round(inss)}\n'
+          f'Desconto do Imposto de Renda de R$ {round(ir)}\n'
           f'Desconto do Sindicato de R$ {round(sindicato)}\n'
-          f'Seu Salario Liquido é de R$ {round(salarioLiquido)}')
-    """
-    
-    print(f'''Seu Salario Bruto é de R$ {round(salarioBruto)}
-Desconto do INSS de R$ {round(INSS)} 
-Desconto do Imposto de Renda de R$ {round(IR)} 
-Desconto do Sindicato de R$ {round(sindicato)} 
-Seu Salario Liquido é de R$ {round(salarioLiquido)}''')
+          f'Seu Salario Liquido é de R$ {round(salario_liquido)}')
 
-"""
-É bacana criar um bloco:
 
 if __name__ == '__main__':
     trabalho()
     descontos()
-    
-Aqui tem a explicação do porquê usar isso:
-https://www.youtube.com/watch?v=F2BC9fwm3tI&t=339s 
-"""
-trabalho()
-descontos()
